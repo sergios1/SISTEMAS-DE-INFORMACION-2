ea7d3ef24f33a35a13e06c153094c6b130b0985b
JonaPool <jonapoolch@gmail.com>
2018-10-02 10:46:15 -0400

    creacion de metodo para la conexion base de datos en la clase conexionBD

--- src/modelo/conexionBD.java
+++ src/modelo/conexionBD.java
@@ -2,4 +2,9 @@ package modelo;
 
 public class conexionBD {
 
+private Conexion crearconexion() throws SQLException ,ClassNotFoundException{
+class.forName ("org.postgres.Driver");
+String url= "jdbc:postgresql://localhost5432/CONFERENCIA";
+Connection conexion= DriverManager.getConnection(url,"postgres","Jonathan");
+return conexion; 
 }
