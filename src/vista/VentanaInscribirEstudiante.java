b1d994863ea48e2285b8d374c731b96bbeef0132
JonaPool <jonapoolch@gmail.com>
2018-10-05 23:50:06 -0400

    Creacion de la ventana de inscripcion y conexion de la bd

--- src/vista/VentanaInscribirEstudiante.java
+++ src/vista/VentanaInscribirEstudiante.java
@@ -1,30 +1,37 @@
 package vista;
-
-import java.awt.Button;
-
 import javax.swing.*;
 
-public class VentanaEstuduante {
-	public static void main (String[]args ) {
-		
-		JFrame s = new JFrame("INSCRIPCION DE ESTUDIANTES")
-		s.setSize(400,300);
-		s.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		s.setVisible(true);
-		
-	}
-}
+import sun.java2d.pipe.hw.AccelDeviceEventListener;
 
-public class VentanaInscribirEstudiante extends JPanel {
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class VentanaInscribirEstudiante extends JFrame implements ActionListener {
 	
-	JLabel labelTitulo, labelNombre, labelApellidoPaterno,labellabelApellidoMaterno,labelCelular,labelCorreoElectronico;
+	private JLabel labelTitulo, labelNombre, labelApellidoPaterno,labellabelApellidoMaterno,labelCelular,labelCorreoElectronico;
 	JTextField fieldNombre,fieldApellidoPaterno,fieldApellidoMAterno,fieldCelular,fieldCorreoElectrinico;
 	JButton botonInscribirse;
 	JButton botonCancelar;
+	private DimensionesPantalla dimPan;
 public VentanaInscribirEstudiante () {
   this.setLayout (null);
+  dimPan = new DimensionesPantalla();
+  this.setBounds(0, 0, dimPan.PenX(50), dimPan.PenY(50));
+  labelTitulo = new JLabel("INSCRIPCION");
+  labelTitulo.setBounds(0, dimPan.PenY(1), dimPan.PenX(50), dimPan.PenY(4));
+  labelTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+  labelTitulo.setFont(new Font("Andale Mono", 1, dimPan.tamanioLetra(25)));
+  add(labelTitulo);
+  
+  
   
 
  }	
+
+public static void main (String[]args ) {
+	
+	new VentanaInscribirEstudiante().setVisible(true);
+	
+}
 }
  
